0a92509a3cd142eafc63368528d3bce2c515291a
Midhath Ahmed <mdhthahmd@gmail.com>
2021-07-21 21:54:43 +0500

    added models

---
+++ src/Ordering.API/Application/Models/CustomerBasket.cs
@@ -0,0 +1,14 @@
+namespace Ordering.API.Application.Models
+{
+    public class CustomerBasket
+    {
+        public string BuyerId { get; set; }
+        public List<BasketItem> Items { get; set; }
+
+        public CustomerBasket(string customerId)
+        {
+            BuyerId = customerId;
+            Items = new List<BasketItem>();
+        }
+    }
+}
